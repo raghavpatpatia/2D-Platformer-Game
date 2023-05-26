3b205ef12ff0727c25e5349b93778e9a3d9e45e2
Raghav Patpatia <raghav.patpatia@gmail.com>
2023-05-27 03:28:21 +0530

    2D-Platformer-Game
    Created horizontal input and added flipping of player as well.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private Animator animator;
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float speed = Input.GetAxisRaw("Horizontal");
+        animator.SetFloat("Speed", Mathf.Abs(speed));
+
+        Vector3 scale = transform.localScale;
+        if (speed > 0) 
+        {
+            scale.x = Mathf.Abs(scale.x);
+        }
+        else if (speed < 0) 
+        {
+            scale.x = -1f * Mathf.Abs(scale.x);
+        }
+        transform.localScale = scale;
     }
 }
