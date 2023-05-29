bc2e1f715bfe1506661e491c7cf277807d05a707
Raghav Patpatia <raghav.patpatia@gmail.com>
2023-05-30 00:59:40 +0530

    2D-Platformer-Game
    Added horizontal movement in the PlayerController script.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     private Animator animator;
+    public float speed;
 
     private void Awake()
     {
@@ -22,19 +24,8 @@ public class PlayerController : MonoBehaviour
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
-        animator.SetFloat("Speed", Mathf.Abs(horizontal));
 
-        // Horizontal character movement (Player_Idle -> Player_Run)
-        Vector3 scale = transform.localScale;
-        if (horizontal > 0) 
-        {
-            scale.x = Mathf.Abs(scale.x);
-        }
-        else if (horizontal < 0) 
-        {
-            scale.x = -1f * Mathf.Abs(scale.x);
-        }
-        transform.localScale = scale;
+        HorizontalMovement(horizontal);
 
         // Vertical Character movement
         if (vertical > 0) 
@@ -50,10 +41,37 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.RightControl) || Input.GetKeyDown(KeyCode.LeftControl)) 
         {
             animator.SetBool("isCrouching", true);
+
         }
         else if (Input.GetKeyUp(KeyCode.RightControl) || Input.GetKeyUp(KeyCode.LeftControl))
         {
             animator.SetBool("isCrouching", false);
         }
+    }
+
+    void PlayerMovementHorizontal(float horizontal) 
+    { 
+        Vector2 position = transform.position;
+        position.x += horizontal * speed * Time.deltaTime;
+        transform.position = position;
+    }
+
+    void HorizontalMovement(float horizontal) 
+    {
+        animator.SetFloat("Speed", Mathf.Abs(horizontal));
+
+        // Horizontal character movement (Player_Idle -> Player_Run)
+        Vector3 scale = transform.localScale;
+        if (horizontal > 0)
+        {
+            scale.x = Mathf.Abs(scale.x);
+            PlayerMovementHorizontal(horizontal);
+        }
+        else if (horizontal < 0)
+        {
+            scale.x = -1f * Mathf.Abs(scale.x);
+            PlayerMovementHorizontal(horizontal);
+        }
+        transform.localScale = scale;
     }
 }
